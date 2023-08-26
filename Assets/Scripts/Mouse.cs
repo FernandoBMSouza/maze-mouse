7c5273e2e963f8d39cb53f095a2d64243316edb4
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2023-08-26 13:11:19 -0300

    Primeiro Commit

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Vector3 targetPosition;
+    private bool isMoving = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetTargetPosition();
+        }
+
+        if (isMoving)
+        {
+            MoveToTarget();
+        }
+    }
+
+    private void SetTargetPosition()
+    {
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition.z = transform.position.z;
+        isMoving = true;
+
+        // Rotate the character towards the target position
+        Vector3 direction = targetPosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if(targetPosition.x < this.transform.position.x)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipY = true;
+        }
+        else{
+            gameObject.GetComponent<SpriteRenderer>().flipY = false;
+        }
+    }
+
+    private void MoveToTarget()
+    {
+        Vector3 moveDirection = (targetPosition - transform.position).normalized;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.05f)
+        {
+            transform.position = targetPosition;
+            isMoving = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isMoving && collision.gameObject.CompareTag("Wall"))
+        {
+            isMoving = false;
+        }
+    }
+}
