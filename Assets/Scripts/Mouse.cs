93060af446b8c93d398e5bdc51661ff5f95f665c
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2023-08-26 16:47:37 -0300

    cenario em tilemap

--- Assets/Scripts/Mouse.cs
+++ Assets/Scripts/Mouse.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Mouse : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 20f;
     private Vector3 targetPosition;
     private bool isMoving = false;
     public int Points { get; set; }
